69519704793151625e3d6225173d982fabdf4c09
Rahul Kumar Barnwal <78400704+rahulgithub-web@users.noreply.github.com>
2022-06-20 22:44:59 +0530

    Update memeController.js
    updated assignments

--- src/controllers/memeController.js
+++ src/controllers/memeController.js
@@ -1,17 +1,39 @@
 let axios = require("axios");
 
-let getMemes = async function(res,req) {
+let getMeme = async function(req, res) {
     try {
-    let memeId = req.query.template_id;
+        var options = {
+            method: "get",
+            url: "https://api.imgflip.com/get_memes"
+        }
+        let results = await axios(options);
+        console.log(results);
+        res.status(200).send({data: results.data});
+    } catch(err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message });
+    }
+}
+
+let getMemes = async function(req,res) {
+    try {
+    let template_id = req.query.template_id;
     let text0 = req.query.text0;
     let text1 = req.query.text1;
-    var options = {
+    // let userName = req.headers["username"]; 
+    // let password = req.headers.password;
+    console.log(template_id, text1, text0);
+    if(template_id && text0 && text1) {
+    let options = {
         method: "post",
-        url: `http://api.imgflip.com/caption_image?template_id=${memeId}&text0=${text0}&text1=${text1}&userName=chewie123456password=meme@123`
+        url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=chewie12345&password=meme@123`
     }
     let result = await axios(options);
     console.log(result.data);  
-    res.status(201).send({ msg: result.data})
+    res.status(200).send({ status: true, msg: result.data })
+    } else {
+        res.status(400).send({status: false, msg:"Please provide valid input data"});
+    } 
   }  catch(err) {
     console.log(err)
     res.status(500).send({ msg: err.message });
@@ -19,3 +41,4 @@ let getMemes = async function(res,req) {
 }
 
 module.exports.getMemes = getMemes;
+module.exports.getMeme = getMeme;
